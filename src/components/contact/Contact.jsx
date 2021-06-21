3b8ea4e387f63970273f394c42abac5626c5dfe4
Arecio Canton <cuba289@gmail.com>
2021-06-21 18:00:07 -0400

    fixed contact page MQ for Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,8 @@ import "./contact.scss";
 const Contact = () => {
   const [state, handleSubmit] = useForm("mqkwbodw");
   if (state.succeeded) {
-    return <h1>Thanks for joining!</h1>;
+    
+    return <h1>Thank you for your feedback!</h1>;
   }
   // const [message, setMessage] = useState(false)
 
@@ -23,7 +24,6 @@ const Contact = () => {
       <div className="right">
           <h2>Contact Me</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email Address</label>
           <input id="email" type="email" name="email" placeholder='Email' />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
           <textarea id="message" name="message" placeholder='Message' />
@@ -41,4 +41,13 @@ const Contact = () => {
   );
 };
 
+{
+  /* <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Email' />
+        <textarea placeholder='Message'></textarea>
+        <button href='mailto:cuba289@gmail.com' type='submit'>Send</button>
+    {message && <span id='message'>Thank you for leaving a message, I'll get back to you ASAP :)</span>} 
+     </form> */
+}
 export default Contact;
